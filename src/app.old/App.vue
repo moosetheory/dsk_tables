d9c8205242d80fa51ebc80f60f64dc4bb9d9c5fc
Annika Merris <annika@adhdgirl.dev>
2025-11-22 16:21:55 -0500

    Trying To Learn Electron

--- src/app.old/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { RouterView } from 'vue-router';
-
-console.log('👋 This message is being logged by "App.vue", included via Vite');
-</script>
-
-<template>
-  <RouterView />
-</template>
